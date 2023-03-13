096610e72a3901e9b3bc50e61b206963549d3518
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-03-13 18:26:54 -0300

    added usercontroller

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+const { objectId } = require('mongoose').Types;
+const { User, Thought } = require('../models')
+
+module.exports = {
+    allUser(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    getSingleUser(req, res) {
+        User.findOne({ __id: req.params.userId })
+            .select('-__v')
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    createUser(req, res) {
+        User.create(req.body)
+            .then((newUser) => res.json(newUser))
+            .catch((e) => res.status(500).json(e));
+    }
+
+}
