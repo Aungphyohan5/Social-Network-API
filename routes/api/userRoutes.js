e4d8f10477bab9c1487d3dff1eb7c228e9d178ac
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-03-14 21:26:36 -0300

    removed thought route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { allUsers, createUser, getSingleUser, updateUser, deleteUser, addFriend, deleteFriend } = require('../../controllers/userController');
-const { route } = require('./thoughtRoutes');
+
 
 router.route('/').get(allUsers).post(createUser);
 
